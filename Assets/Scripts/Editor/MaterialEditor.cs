0d183e831af2b264ee1587f3c1a81762baec44fd
Aleksandr Usachev <aleksandr.usachev@plarium.com>
2017-06-19 10:51:36 +0300

    add IronScheme dlls

---
+++ Assets/Scripts/Editor/MaterialEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeEdit
+{
+    public class MaterialEditor : ShaderGUI
+    {
+        private bool _folded = true;
+        private Editor _cachedEditor;
+
+        public override void OnGUI(UnityEditor.MaterialEditor materialEditor, MaterialProperty[] properties)
+        {
+            if (!_cachedEditor)
+            {
+                var material = materialEditor.target as Material;
+                if (material != null)
+                {
+                    var shader = material.shader;
+                    var generators = Utils.FindAllAssets<ShaderGenerator>();
+                    var targetGenerator = generators.FirstOrDefault(generator => generator.ShaderReference == shader);
+                    if (targetGenerator)
+                    {
+                        _cachedEditor = Editor.CreateEditor(targetGenerator);
+                    }
+                }
+            }
+
+            if (_cachedEditor)
+            {
+                _cachedEditor.OnInspectorGUI();
+                EditorGUILayout.Space();
+            }
+
+            _folded = Utils.Foldout("Material Properties", _folded);
+            if (_folded)
+            {
+                ++EditorGUI.indentLevel;
+                base.OnGUI(materialEditor, properties);
+                --EditorGUI.indentLevel;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Raymarching/RaymarchingObject.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode, RequireComponent(typeof(Renderer))]
+public class RaymarchingObject : MonoBehaviour
+{
+    public enum Shape
+    {
+        Cube,
+        Sphere,
+        None,
+    }
+
+    [SerializeField]
+    Shape shape = Shape.Cube;
+
+    [SerializeField]
+    Color GizmoColor = new Color(1f, 1f, 1f, 0.1f);
+
+    [SerializeField] Color GizmoSelectedColor = new Color(1f, 0f, 0f, 1f);
+
+    private int _scaleId;
+    private Material _material;
+
+    private Vector3 Scale
+    {
+        get
+        {
+            var s = transform.localScale;
+            return new Vector3(Mathf.Abs(s.x), Mathf.Abs(s.y), Mathf.Abs(s.z));
+        }
+    }
+
+    void Awake()
+    {
+        _scaleId = Shader.PropertyToID("_Scale");
+        _material = GetComponent<Renderer>().sharedMaterial;
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        _material = GetComponent<Renderer>().sharedMaterial;
+#endif
+        UpdateScale();
+        UpdateShape();
+    }
+
+    private void UpdateScale()
+    {
+        _material.SetVector(_scaleId, Scale);
+    }
+
+    private void UpdateShape()
+    {
+        switch (shape)
+        {
+             case Shape.Cube:
+                _material.EnableKeyword("OBJECT_SHAPE_CUBE");
+                _material.DisableKeyword("OBJECT_SHAPE_SPHERE");
+                break;
+            case Shape.Sphere:
+                _material.EnableKeyword("OBJECT_SHAPE_SPHERE");
+                _material.DisableKeyword("OBJECT_SHAPE_CUBE");
+                break;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        DrawGizmos(GizmoColor);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        DrawGizmos(GizmoSelectedColor);
+    }
+
+    private void DrawGizmos(Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.matrix = Matrix4x4.identity * transform.localToWorldMatrix;
+        switch (shape)
+        {
+            case Shape.Cube:
+                Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+                break;
+            case Shape.Sphere:
+                Gizmos.DrawWireSphere(Vector3.zero, 0.5f);
+                break;
+            case Shape.None:
+                break;
+        }
+    }
+}
